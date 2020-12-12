c8d463a08e9a4a0251e38488431d7293cd3bdfb2
Galileo Luna <galileoungs@gmail.com>
2020-12-12 18:04:56 -0300

    probamos

--- src/main/java/conect/Initialize.java
+++ src/main/java/conect/Initialize.java
@@ -5,6 +5,19 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+
+
 import org.json.JSONArray;
 
 import servicios.BuscadorBasesDeDatos;
@@ -15,6 +28,26 @@ public class Initialize {
 		BuscadorBasesDeDatos findMongo = new BuscadorBasesDeDatos();
 		File f = new File("./services/mongo/mongoproyect.jar");
 		Class conec = BuscadorBasesDeDatos.findDataBases(f).get(0);
+		Method metodos[] = conec.getMethods();
+		int tmn =metodos.length;
+		Method a= null;
+		Method b= null;
+		Method c= null;
+		Method d= null;
+		Method[] finales = {a,b,c,d};
+		
+		for(int i=0;i<tmn-1;i++) {
+			System.out.println(metodos[i].toString());
+			if(tmn<4) {
+				finales[i]=metodos[i];
+			}
+		}
+		return finales;
+	}
+	public Method[] getMongoQuerys() {
+		BuscadorBasesDeDatos findMongo = new BuscadorBasesDeDatos();
+		File f = new File("./services/mongo/mongoproyect.jar");
+		Class conec = BuscadorBasesDeDatos.findDataBases(f).get(1);
 		Method metodos[] = conec.getMethods();
 		//"mongodb+srv://admin:admin@cluster0.ui4uq.mongodb.net/STU?retryWrites=true&w=majority"
 		return metodos;
@@ -29,7 +62,6 @@ public class Initialize {
 		Method b= null;
 		Method[] finales = {a,b};
 		for(int i=0;i<tmn-1;i++) {
-			System.out.println(metodos[i].toString());
 			if(metodos[i].toString().equalsIgnoreCase("public org.json.JSONArray stublockchain.ApiGateway.getInscripciones(java.lang.String) throws java.io.IOException")) {
 				finales[0]=metodos[i];
 			}
@@ -44,7 +76,7 @@ public class Initialize {
 		File f = new File("./services/mongo/mongoproyect.jar");
 		ArrayList<Class> conec =BuscadorBasesDeDatos.findDataBases(f);
 		//6 APi gateway
-		Method[] metodos= conec.get(9).getMethods();
+		Method[] metodos= conec.get(0).getMethods();
 		int tmn =metodos.length;
 		Method a= null;
 		Method b= null;
